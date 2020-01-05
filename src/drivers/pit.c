2508371b4d4c2f238b0e0dc527469468277481e0
UniverseSquared <universesquared4@gmail.com>
2020-01-05 14:23:55 +0000

    Set the PIT interrupt handler before configuring the timer frequency

--- src/drivers/pit.c
+++ src/drivers/pit.c
@@ -10,13 +10,13 @@ static void pit_timer_callback(cpu_state_t cpu_state) {
 }
 
 void init_pit(void) {
+    /* Register our callback. */
+    set_interrupt_handler(IRQ(0), pit_timer_callback);
+
     /* Calculate the divisor required to get a frequency of 100Hz. */
     uint32_t divisor = 1193180 / 100;
 
     port_write_byte(PIT_COMMAND_PORT,       0x36);
     port_write_byte(PIT_CHANNEL0_DATA_PORT, divisor & 0xFF);
     port_write_byte(PIT_CHANNEL0_DATA_PORT, (divisor >> 8) & 0xFF);
-
-    /* Register our callback. */
-    set_interrupt_handler(IRQ(0), pit_timer_callback);
 }
